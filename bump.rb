ec341b27692ed606d72efd422ea93e9da5089db8
boertel <benjamin.oertel@gmail.com>
2016-04-19 19:13:36 -0700

    update sha

--- bump.rb
+++ bump.rb
@@ -2,7 +2,7 @@ class Bump < Formula
     desc "dumb bumping version command"
     homepage "https://github.com/boertel/bump"
     url "https://github.com/boertel/bump/archive/v1.0.1.tar.gz"
-    sha256 "44ff32060a8433dc861cf184af63b183fbdd70d2"
+    sha256 "903e0e53b24b420aa778525c7ae75f0b9d8cf1e292c4a29af44f0556ea2bdeb2"
     head "https://github.com/boertel/bump.git"
 
     bottle :unneeded
