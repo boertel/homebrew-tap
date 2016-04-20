7f304d44e624c6397d7b0885d4309d09a8dc3ab7
boertel <benjamin.oertel@gmail.com>
2016-04-19 19:19:58 -0700

    finally good sha

--- bump.rb
+++ bump.rb
@@ -2,7 +2,7 @@ class Bump < Formula
     desc "dumb bumping version command"
     homepage "https://github.com/boertel/bump"
     url "https://github.com/boertel/bump/archive/v1.0.1.tar.gz"
-    sha256 "903e0e53b24b420aa778525c7ae75f0b9d8cf1e292c4a29af44f0556ea2bdeb2"
+    sha256 "84e909a42484512f58b59f635d212e9899944bf1c629c2c4cc4f0a51797addc3"
     head "https://github.com/boertel/bump.git"
 
     bottle :unneeded
