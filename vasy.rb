eb71eea88d9a2850a818b27472192bf6c69ef2d9
boertel <benjamin.oertel@gmail.com>
2017-02-23 15:50:19 -0800

    Update to vasy to 2.1.0

--- vasy.rb
+++ vasy.rb
@@ -1,14 +1,13 @@
 class Vasy < Formula
     desc "vas-y, quick cd with aliases"
     homepage "https://github.com/boertel/vasy"
-    url "https://github.com/boertel/vasy/archive/latest.tar.gz"
-    sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+    url "https://github.com/boertel/vasy/archive/v2.1.0.tar.gz"
+    sha256 "439c6367f60dbf30ea4d69332d96e074b69202218a82a5634191bdfb125f9735"
     head "https://github.com/boertel/vasy.git"
 
     bottle :unneeded
 
     def install
-        bin.install "vasy-resolve"
         system "./install.sh"
     end
 end
