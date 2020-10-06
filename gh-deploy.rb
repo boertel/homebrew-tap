006c65f8d8929d047e72cf9962bbc0704463f997
boertel <ben@comediadesign.com>
2020-10-06 09:05:40 -0700

    add gh-deploy

---
+++ gh-deploy.rb
@@ -0,0 +1,13 @@
+class Gh-deploy < Formula
+    desc ""
+    homepage "https://github.com/boertel/gh-deploy"
+    url "https://github.com/boertel/gh-deploy/archive/v1.0.0.tar.gz"
+    sha256 "6798245964a4a4a3e4b5a6667e5ea153b4fb56737d807a609cbd6321611a6b2a"
+    head "https://github.com/boertel/gh-deploy.git"
+
+    bottle :unneeded
+
+    def install
+        bin.install gh-deploy
+    end
+end
