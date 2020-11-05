2821784928c87fce26e1752efac2a4278de2adf0
boertel <ben@comediadesign.com>
2020-11-04 16:24:36 -0800

    update gh-deploy

--- gh-deploy.rb
+++ gh-deploy.rb
@@ -1,8 +1,8 @@
 class GhDeploy < Formula
     desc ""
     homepage "https://github.com/boertel/gh-deploy"
-    url "https://github.com/boertel/gh-deploy/archive/v1.1.1.tar.gz"
-    sha256 "2b5c5e831abb9448e77c952b3c75e3f1763e5163198e6b4a24bdb82e1452597b"
+    url "https://github.com/boertel/gh-deploy/archive/v1.1.2.tar.gz"
+    sha256 "dd4666c88d6e8ca6180702c520966f2147bf5a8bfa5a95dffe353d8c38a6ab24"
     head "https://github.com/boertel/gh-deploy.git"
 
     bottle :unneeded
