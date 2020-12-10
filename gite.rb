1077fa47567fadaa4f4692e926d3f1efcf1f7a1a
boertel <ben@comediadesign.com>
2020-12-10 08:25:53 -0800

    update tap

--- gite.rb
+++ gite.rb
@@ -1,8 +1,8 @@
 class Gite < Formula
     desc ""
     homepage "https://github.com/boertel/gite"
-    url "https://github.com/boertel/gite/archive/v1.1.0.tar.gz"
-    sha256 "cff9727283a459e8cecde82248e93bd0d84817792d0877d5cd54b1dd3c4adce9"
+    url "https://github.com/boertel/gite/archive/v1.2.0.tar.gz"
+    sha256 "ac80a60fd6d6057d91fb40e98d6b8b337bb564709c1968b05e1eb585f05950fa"
     head "https://github.com/boertel/gite.git"
 
     bottle :unneeded
