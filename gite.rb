4dbfe52577937ca3d85b324a7cdbcaade19343fe
boertel <ben@comediadesign.com>
2020-12-10 08:34:41 -0800

    Bump gite to v1.2.7

--- gite.rb
+++ gite.rb
@@ -1,8 +1,8 @@
 class Gite < Formula
     desc ""
     homepage "https://github.com/boertel/gite"
-    url "https://github.com/boertel/gite/archive/v1.2.6.tar.gz"
-    sha256 "58a2ca9876e4039540018107aded0cd2a4b4700a221a0a976db76ce768d0e8f7"
+    url "https://github.com/boertel/gite/archive/v1.2.7.tar.gz"
+    sha256 "e6c881de7d6c57a7f7f5e6302322aa2e910fe40482d6480483c8b7c1e793fbc7"
     head "https://github.com/boertel/gite.git"
 
     bottle :unneeded
