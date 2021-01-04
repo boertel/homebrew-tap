98c7ff602a79586ea3754d6525631d9061059c53
boertel <ben@comediadesign.com>
2021-01-04 11:23:58 -0800

    Update vasy with autocomplete

--- vasy.rb
+++ vasy.rb
@@ -1,8 +1,8 @@
 class Vasy < Formula
     desc "vas-y, quick cd with aliases"
     homepage "https://github.com/boertel/vasy"
-    url "https://github.com/boertel/vasy/archive/v2.1.0.tar.gz"
-    sha256 "439c6367f60dbf30ea4d69332d96e074b69202218a82a5634191bdfb125f9735"
+    url "https://github.com/boertel/vasy/archive/v2.2.0.tar.gz"
+    sha256 "6dd1aa65d497afae5a172835b86af30f22611c0a514aba63ebb7d153bba59ced"
     head "https://github.com/boertel/vasy.git"
 
     bottle :unneeded
