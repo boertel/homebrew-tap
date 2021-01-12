61cfa90918526d69a0fa879e60b3efe3746a109d
boertel <ben@comediadesign.com>
2021-01-12 12:51:08 -0800

    Updating t

--- t.rb
+++ t.rb
@@ -1,8 +1,8 @@
 class T < Formula
     desc ""
     homepage "https://github.com/boertel/t"
-    url "https://github.com/boertel/t/archive/v0.1.0.tar.gz"
-    sha256 "c22d3e9cffd9ffc4ef58b3ea3fa60ed10660639b8b95bd89b04f993c50f2af7c"
+    url "https://github.com/boertel/t/archive/v0.1.1.tar.gz"
+    sha256 "a33bec23e9cecbea7d014a312091c6490191a2364b4c52cbbf54b6152c5dfb7c"
     head "https://github.com/boertel/t.git"
 
     bottle :unneeded
