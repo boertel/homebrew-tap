defe61205040ea435132ac526e9d5f140a2ab8ea
boertel <ben@comediadesign.com>
2021-01-12 11:45:27 -0800

    hard code bump version for now

--- bump.rb
+++ bump.rb
@@ -1,8 +1,8 @@
 class Bump < Formula
     desc "dumb bumping version command"
     homepage "https://github.com/boertel/bump"
-    url "https://github.com/boertel/bump/archive/latest.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    url "https://github.com/boertel/bump/archive/v1.1.2.tar.gz"
+    sha256 "324cf306f6bbb838f8dc0e28a66449dc766bc33c17aeb3e9b7d4ebab8dd1e519"
     head "https://github.com/boertel/bump.git"
 
     bottle :unneeded
