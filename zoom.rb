70a50cbfd285cdba0cfc7e28236b953d03581af8
boertel <ben@comediadesign.com>
2021-01-28 10:28:40 -0800

    Bump zoom to v0.0.0

---
+++ zoom.rb
@@ -0,0 +1,13 @@
+class Zoom < Formula
+    desc ""
+    homepage "https://github.com/boertel/zoom"
+    url "https://github.com/boertel/zoom/archive/v0.0.0.tar.gz"
+    sha256 "87955e92ab81b0e0e67358237ca5f4f65c708e67b420cedef65aa7565c78b335"
+    head "https://github.com/boertel/zoom.git"
+
+    bottle :unneeded
+
+    def install
+        
+    end
+end
