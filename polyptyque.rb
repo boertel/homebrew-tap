eb7f4f724f8e91ffe9a314508d755c5a483ecf41
boertel <ben@comediadesign.com>
2021-10-09 16:26:31 -0700

    Bump polyptyque to v0.1.1

--- polyptyque.rb
+++ polyptyque.rb
@@ -1,8 +1,8 @@
 class Polyptyque < Formula
     desc ""
     homepage "https://github.com/boertel/polyptyque"
-    url "https://github.com/boertel/polyptyque/archive/v0.1.0.tar.gz"
-    sha256 "5bf2a1c94bd92323fbd30899263c3116322cec456644c2be5de2a114010cfd23"
+    url "https://github.com/boertel/polyptyque/archive/v0.1.1.tar.gz"
+    sha256 "fa7f0927b01f6d67323f55bebb0a29930bd6efee18dbd938b506634b508ed2f9"
     head "https://github.com/boertel/polyptyque.git"
 
     bottle :unneeded
