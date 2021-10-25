b9787e3960d8e2f6958d2e7ff507e65772fac36c
boertel <ben@comediadesign.com>
2021-10-25 12:55:12 -0700

    Bump vasy to v2.2.18

--- vasy.rb
+++ vasy.rb
@@ -1,14 +1,14 @@
 class Vasy < Formula
     desc "vas-y, quick cd with aliases"
     homepage "https://github.com/boertel/vasy"
-    url "https://github.com/boertel/vasy/archive/v2.2.17.tar.gz"
-    sha256 "18db2cdbe196209b7e8e3964ea925117900a7b7d9dea97d47a6d33855ae64472"
+    url "https://github.com/boertel/vasy/archive/v2.2.18.tar.gz"
+    sha256 "3c8f5cb5961a0bc6c24bf9d9ab656fc6f54897d880382a01ae0b2aac34c41b7e"
     head "https://github.com/boertel/vasy.git"
 
     def install
         bin.install Dir["bin/*"]
         lib.install Dir["lib/*"]
-        zsh_completion.install "vasy-completion.zsh" => "_vasy"
+        zsh_completion.install "completions/vasy.zsh" => "_vasy"
     end
 
     def caveats
