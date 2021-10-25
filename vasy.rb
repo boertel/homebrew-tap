6fdb3647cd28a6c66f98e290ad4500e2d66e6787
boertel <ben@comediadesign.com>
2021-10-25 09:47:18 -0700

    Updating vasy

--- vasy.rb
+++ vasy.rb
@@ -1,13 +1,14 @@
 class Vasy < Formula
     desc "vas-y, quick cd with aliases"
     homepage "https://github.com/boertel/vasy"
-    url "https://github.com/boertel/vasy/archive/v2.2.0.tar.gz"
-    sha256 "6dd1aa65d497afae5a172835b86af30f22611c0a514aba63ebb7d153bba59ced"
+    url "https://github.com/boertel/vasy/archive/v2.2.1.tar.gz"
+    sha256 "28a3cdbbcc41644e8c5dd6e1899d4e3a55cbdbf8ee9834abb99c7e311beb24a2"
     head "https://github.com/boertel/vasy.git"
 
     bottle :unneeded
 
     def install
-        system "./install.sh"
+        bin.install_symlink Dir["#{libexec}/bin/*"]
+        zsh_completion.install libexec/"vasy-autocompletion.zsh"
     end
 end
