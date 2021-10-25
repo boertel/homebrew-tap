a6fee0a57f9c2218eb0fec22c67d24225a456a68
boertel <ben@comediadesign.com>
2021-10-25 11:52:47 -0700

    Bump vasy to v2.2.12

--- vasy.rb
+++ vasy.rb
@@ -1,12 +1,12 @@
 class Vasy < Formula
     desc "vas-y, quick cd with aliases"
     homepage "https://github.com/boertel/vasy"
-    url "https://github.com/boertel/vasy/archive/v2.2.11.tar.gz"
-    sha256 "35591b06577efbae2eb647be32f5b83e8e41db02330435a4e6657a7609d2bea7"
+    url "https://github.com/boertel/vasy/archive/v2.2.12.tar.gz"
+    sha256 "1a640a45aa92dbca45e62800da6a6680895695e32364e24dabb081545a004d4d"
     head "https://github.com/boertel/vasy.git"
 
     def install
         bin.install Dir["bin/*"]
-        system "./install.sh", "${bin}"
+        system "./install.sh", "#{bin}"
     end
 end
