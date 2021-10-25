0e4ec5f25523c31673de9071a12b67f518312270
boertel <ben@comediadesign.com>
2021-10-25 10:14:44 -0700

    Updating vasy

--- vasy.rb
+++ vasy.rb
@@ -1,11 +1,11 @@
 class Vasy < Formula
     desc "vas-y, quick cd with aliases"
     homepage "https://github.com/boertel/vasy"
-    url "https://github.com/boertel/vasy/archive/v2.2.2.tar.gz"
-    sha256 "b97fd018f8778cf85f9bf1b693ab623e20b20120cdd52182aa37e95a481faf46"
+    url "https://github.com/boertel/vasy/archive/v2.2.5.tar.gz"
+    sha256 "e3b355fd041dce944dc79e49648521cd6e7238cb0e4dfdbf968407ccf2c6a8cc"
     head "https://github.com/boertel/vasy.git"
 
     def install
-        bin.install_symlink Dir["#{libexec}/bin/*"]
+        bin.install Dir["bin/*"]
     end
 end
