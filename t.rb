74fa8d7c467ebaa0ffb097ab706d35fadc21df56
boertel <ben@comediadesign.com>
2022-06-15 20:23:37 -0700

    Bump t to v0.1.5

--- t.rb
+++ t.rb
@@ -1,11 +1,12 @@
 class T < Formula
     desc ""
     homepage "https://github.com/boertel/t"
-    url "https://github.com/boertel/t/archive/v0.1.3.tar.gz"
-    sha256 "e7222e05d888fe3088f7a25b71feb79f16926fb28e5223a9f379b1a9fdcb2ce3"
+    url "https://github.com/boertel/t/archive/v0.1.5.tar.gz"
+    sha256 "70327ae1abc7b10499d57723269ce292d6f33cdf2436de46e3256fe36f12dcd8"
     head "https://github.com/boertel/t.git"
 
     def install
         bin.install "t"
     end
+
 end
