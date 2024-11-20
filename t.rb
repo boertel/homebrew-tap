f39df1648e43c892420897c2c87e8a5e5c5c4302
boertel <ben@comediadesign.com>
2024-11-20 15:54:52 -0800

    Updating t

--- t.rb
+++ t.rb
@@ -1,8 +1,8 @@
 class T < Formula
     desc ""
     homepage "https://github.com/boertel/t"
-    url "https://github.com/boertel/t/archive/v0.1.6.tar.gz"
-    sha256 "239a2341a8763151fd1f0e736e8e1002268ea2a4d1df0614b2166e27a9275d98"
+    url "https://github.com/boertel/t/archive/v0.1.7.tar.gz"
+    sha256 "5828e520a9abd672fb463261d2ebfbcafea738e699aae4905dc1519ee6736e29"
     head "https://github.com/boertel/t.git"
 
     def install
